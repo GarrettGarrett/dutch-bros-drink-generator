88c59e4e04551565fdf583823eb6b870ca6cb3ea
PyPy <skfelix1@gmail.com>
2023-03-18 09:59:02 -0700

    formats code, adds styling + bounce to randomize button

--- components/RandomizeButton.js
+++ components/RandomizeButton.js
@@ -10,7 +10,7 @@ function RandomizeButton({randomize, setRandomize, setWayBackVisible, setNewestI
             setRandomize(!randomize)
             setImageLoading(true)
         }}
-        className='bg-[#365F95] text-white font-Futura flex px-4 py-2 rounded-md hover:cursor-pointer hover:bg-opacity-80 text-center'>
+        className='mb-4 active:scale-95 bg-[#365F95] text-white font-Futura flex px-4 py-2 rounded-md hover:cursor-pointer hover:bg-opacity-80 text-center'>
             <span className='uppercase m-auto'>{buttonText}</span>
         </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import Head from 'next/head'
 import Image from 'next/image'
 import CategoryCard from '../components/CategoryCard'
@@ -7,199 +6,265 @@ import SelectedDrink from '../components/SelectedDrink'
 import { useState, useEffect } from 'react'
 import Header from '../components/Header'
 
-function addBracketsIfNEeded(string, currentIndex, totalArrayLength){
+function addBracketsIfNEeded(string, currentIndex, totalArrayLength) {
   if (string?.length > 1) {
-      let firstChar  = string[0]
-      let lastChar = string[string.length - 1]
-      let editableString = string
+    let firstChar = string[0]
+    let lastChar = string[string.length - 1]
+    let editableString = string
 
-      if (firstChar != "{") {
-          editableString = "{" + editableString
-      }
-      if (currentIndex == totalArrayLength - 1) {
-          editableString = editableString  + ':1' 
-      }
-      if (lastChar != "}") {
-          editableString = editableString  + '}'
-      }
-      return editableString
+    if (firstChar != '{') {
+      editableString = '{' + editableString
+    }
+    if (currentIndex == totalArrayLength - 1) {
+      editableString = editableString + ':1'
+    }
+    if (lastChar != '}') {
+      editableString = editableString + '}'
+    }
+    return editableString
   }
- return string
+  return string
 }
 
 export async function getServerSideProps(context) {
-  var myHeaders = new Headers();
-  myHeaders.append("authority", "www.dutchbros.com");
-  myHeaders.append("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-  myHeaders.append("accept-language", "en-US,en;q=0.9");
-  myHeaders.append("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"");
-  myHeaders.append("sec-ch-ua-mobile", "?0");
-  myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
-  myHeaders.append("sec-fetch-dest", "document");
-  myHeaders.append("sec-fetch-mode", "navigate");
-  myHeaders.append("sec-fetch-site", "cross-site");
+  var myHeaders = new Headers()
+  myHeaders.append('authority', 'www.dutchbros.com')
+  myHeaders.append(
+    'accept',
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+  )
+  myHeaders.append('accept-language', 'en-US,en;q=0.9')
+  myHeaders.append(
+    'sec-ch-ua',
+    '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"'
+  )
+  myHeaders.append('sec-ch-ua-mobile', '?0')
+  myHeaders.append('sec-ch-ua-platform', '"macOS"')
+  myHeaders.append('sec-fetch-dest', 'document')
+  myHeaders.append('sec-fetch-mode', 'navigate')
+  myHeaders.append('sec-fetch-site', 'cross-site')
 
   var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-  };
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow',
+  }
 
   const resp = await fetch(`https://www.dutchbros.com/menu`, requestOptions)
 
   if (resp) {
-      let res_text = await resp.text()
-      var result = res_text.match(/({"name":(.*)"pdp")/gs)
-      let splitArray = result[0].split("},{")
-      let objectArray = []
-      splitArray.forEach((arr, index) => {
-          let formated = addBracketsIfNEeded(arr, index, splitArray.length)
-          let formatedJson = JSON.parse(formated)
-          if (formatedJson.category != "It's All Goodies") {
-            objectArray.push(JSON.parse(formated))
-          }
-      })
-      return {
-        props: {objectArray}, 
+    let res_text = await resp.text()
+    var result = res_text.match(/({"name":(.*)"pdp")/gs)
+    let splitArray = result[0].split('},{')
+    let objectArray = []
+    splitArray.forEach((arr, index) => {
+      let formated = addBracketsIfNEeded(arr, index, splitArray.length)
+      let formatedJson = JSON.parse(formated)
+      if (formatedJson.category != "It's All Goodies") {
+        objectArray.push(JSON.parse(formated))
       }
-    }                  
+    })
+    return {
+      props: { objectArray },
+    }
+  }
 }
 
-const cards =[
-  {category: "Featured Drinks", description: "Featured", image: "/images/Featured.png"},
-  {category: "Dutch Classics", description: "Our favorites, guaranteed to satisfy", image: "/images/Classics.png"},
-  {category: "Cold Brew", description: "Cold Brew to fuel your day.  Also available Nitro-infused if you need an extra kick.", image: "/images/Coldbrew.png"},
-  {category: "Dutch Freeze", description: "Blended coffee perfection, paired with your favorite flavor.", image: "/images/Dutchfreeze.png"},
-  {category: "Americano", description: "Hand-pulled espresso shots of our dutch bros private reserve coffee blend.", image: "/images/Americano.png"},
-  {category: "Dutch Bros Rebel Energy Drink", description: "An energy drink created by dutch bros, customized by you. Also available in sugar free!", image: "/images/Rebel.png"},
-  {category: "Dutch Frost", description: "A dutch take on a classic shake.", image: "/images/Dutchfrost.png"},
-  {category: "Tea", description: "High-quality organic green or paris black tea infused with any flavor.", image: "/images/Tea.png"},
-  {category: "Kids", description: "Crafted for the kiddos - dutch frosts, smoothies, sodas and not-so-hots.", image: "/images/Kids.png"},
-  {category: "Smoothie", description: "Our fruit smoothie crafted in your favorite flavors.", image: "/images/Smoothie.png"},
-  {category: "Lemonade", description: "A sweet and tart way to refresh your day.", image: "/images/Lemonade.png"},
-  {category: "Dutch Soda", description: "Sparkling soda water infused with your favorite flavor combo.", image: "/images/Dutchsoda.png"},
-  {category: "Chai", description: "Chai it your way! Try it hot or iced!", image: "/images/Chai.png"},
-  {category: "Dutch Cocoa", description: "Our exclusive chocolate milk, steamed to perfection with a flavor of your choice.", image: "/images/Dutchcocoa.png"},
+const cards = [
+  {
+    category: 'Featured Drinks',
+    description: 'Featured',
+    image: '/images/Featured.png',
+  },
+  {
+    category: 'Dutch Classics',
+    description: 'Our favorites, guaranteed to satisfy',
+    image: '/images/Classics.png',
+  },
+  {
+    category: 'Cold Brew',
+    description:
+      'Cold Brew to fuel your day.  Also available Nitro-infused if you need an extra kick.',
+    image: '/images/Coldbrew.png',
+  },
+  {
+    category: 'Dutch Freeze',
+    description: 'Blended coffee perfection, paired with your favorite flavor.',
+    image: '/images/Dutchfreeze.png',
+  },
+  {
+    category: 'Americano',
+    description:
+      'Hand-pulled espresso shots of our dutch bros private reserve coffee blend.',
+    image: '/images/Americano.png',
+  },
+  {
+    category: 'Dutch Bros Rebel Energy Drink',
+    description:
+      'An energy drink created by dutch bros, customized by you. Also available in sugar free!',
+    image: '/images/Rebel.png',
+  },
+  {
+    category: 'Dutch Frost',
+    description: 'A dutch take on a classic shake.',
+    image: '/images/Dutchfrost.png',
+  },
+  {
+    category: 'Tea',
+    description:
+      'High-quality organic green or paris black tea infused with any flavor.',
+    image: '/images/Tea.png',
+  },
+  {
+    category: 'Kids',
+    description:
+      'Crafted for the kiddos - dutch frosts, smoothies, sodas and not-so-hots.',
+    image: '/images/Kids.png',
+  },
+  {
+    category: 'Smoothie',
+    description: 'Our fruit smoothie crafted in your favorite flavors.',
+    image: '/images/Smoothie.png',
+  },
+  {
+    category: 'Lemonade',
+    description: 'A sweet and tart way to refresh your day.',
+    image: '/images/Lemonade.png',
+  },
+  {
+    category: 'Dutch Soda',
+    description:
+      'Sparkling soda water infused with your favorite flavor combo.',
+    image: '/images/Dutchsoda.png',
+  },
+  {
+    category: 'Chai',
+    description: 'Chai it your way! Try it hot or iced!',
+    image: '/images/Chai.png',
+  },
+  {
+    category: 'Dutch Cocoa',
+    description:
+      'Our exclusive chocolate milk, steamed to perfection with a flavor of your choice.',
+    image: '/images/Dutchcocoa.png',
+  },
 ]
 
-const Home = ({objectArray}) => {
-const [randomize, setRandomize] = useState(false)
-const [randomResult, setRandomResult] = useState(getRandomDrink(objectArray))
-const [wayBackVisible, setWayBackVisible ] = useState(true)
-const [newestImageVisible, setNewestImageVisible] = useState(true)
-const [imageLoading, setImageLoading] = useState(false)
-const [selectedCategory, setSelectedCategory] = useState(null)
-const [buttonText, setButtonText] = useState("Randomize")
+const Home = ({ objectArray }) => {
+  const [randomize, setRandomize] = useState(false)
+  const [randomResult, setRandomResult] = useState(getRandomDrink(objectArray))
+  const [wayBackVisible, setWayBackVisible] = useState(true)
+  const [newestImageVisible, setNewestImageVisible] = useState(true)
+  const [imageLoading, setImageLoading] = useState(false)
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  const [buttonText, setButtonText] = useState('Randomize')
 
+  useEffect(() => {
+    setRandomResult(getRandomDrink(objectArray, selectedCategory))
+  }, [randomize])
 
-useEffect(() => {
-  setRandomResult(getRandomDrink(objectArray, selectedCategory))
-}, [randomize])
+  useEffect(() => {
+    setButtonText(
+      `${
+        selectedCategory?.length > 0
+          ? 'Random ' +
+            selectedCategory.replace('Dutch Bros Rebel Energy', 'Rebel')
+          : 'Randomize'
+      }`
+    )
+  }, [selectedCategory])
 
-useEffect(() => {
-  setButtonText(`${selectedCategory?.length > 0 ? "Random " + selectedCategory.replace(
-    "Dutch Bros Rebel Energy", "Rebel"
-  ) : "Randomize"}`)
-}, [selectedCategory])
+  function getRandomDrink(allDrinks, category = null) {
+    let randomNum = randomIntFromInterval(0, allDrinks.length)
+    let randomDrink = allDrinks[randomNum]
 
-
-
-function getRandomDrink(allDrinks, category=null){
-  let randomNum = randomIntFromInterval(0, allDrinks.length)
-  let randomDrink = allDrinks[randomNum]
-
-  if (category == null) { //no category given, choose random without category
-    return randomDrink
+    if (category == null) {
+      //no category given, choose random without category
+      return randomDrink
+    } else {
+      while (randomDrink?.category != category) {
+        randomNum = randomIntFromInterval(0, allDrinks.length)
+        randomDrink = allDrinks[randomNum]
+      }
+      return randomDrink
+    }
   }
-  else {
-    while (randomDrink?.category != category) {
-      randomNum = randomIntFromInterval(0, allDrinks.length)
-      randomDrink = allDrinks[randomNum]
-   }
-   return randomDrink
+
+  function randomIntFromInterval(min, max) {
+    // min and max included
+    let randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+    return randomNumber
   }
-  
-  
-}
 
-function randomIntFromInterval(min, max) { // min and max included 
-  let randomNumber =  Math.floor(Math.random() * (max - min + 1) + min)
-  return randomNumber
-}
-
-
-return (
+  return (
     <>
-      <div className='sticky top-0 z-50'>
-          <Header />
+      <div className="sticky top-0 z-50">
+        <Header />
       </div>
-        <div className="relative max-w-7xl mx-auto px-6 sm:px-6 lg:px-8">
-          <div className="max-w-3xl mx-auto">
-              <div className='py-10'>
-                <div className=''>
-                  <SelectedDrink 
-                    drink={randomResult}
-                    wayBackVisible={wayBackVisible}
-                    setWayBackVisible={setWayBackVisible}
-                    newestImageVisible={newestImageVisible}
-                    setNewestImageVisible={setNewestImageVisible}
-                    imageLoading={imageLoading}
-                    setImageLoading={setImageLoading}
-                  />
-                </div>
-
-                <div className=''>
-                  <RandomizeButton 
-                    randomize={randomize}
-                    setRandomize={setRandomize}
-                    setWayBackVisible={setWayBackVisible}
-                    setNewestImageVisible={setNewestImageVisible}
-                    setImageLoading={setImageLoading}
-                    selectedCategory={selectedCategory}
-                    buttonText={buttonText}
-                  />
-                </div>
-                
-                {
-                  cards.map( (card, index ) => {
-                    return (
-                      <CategoryCard
-                        key={index}
-                        index={index} 
-                        category={card.category}
-                        description={card.description}
-                        image={card.image}
-                        setSelectedCategory={setSelectedCategory}
-                        selectedCategory={selectedCategory}
-                      />
-                    )
-                  })
-                }
-              
-              </div>
+      <div className="relative mx-auto max-w-7xl px-6 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl">
+          <div className="py-10">
+            <div className="">
+              <SelectedDrink
+                drink={randomResult}
+                wayBackVisible={wayBackVisible}
+                setWayBackVisible={setWayBackVisible}
+                newestImageVisible={newestImageVisible}
+                setNewestImageVisible={setNewestImageVisible}
+                imageLoading={imageLoading}
+                setImageLoading={setImageLoading}
+              />
             </div>
 
-            {/* BG Icons */}
-            <div  className="-z-[1] absolute left-1 top-[60vh] h-5 w-5">
-                <img src="/images/windmill.png" alt=""/>
+            <div className="">
+              <RandomizeButton
+                randomize={randomize}
+                setRandomize={setRandomize}
+                setWayBackVisible={setWayBackVisible}
+                setNewestImageVisible={setNewestImageVisible}
+                setImageLoading={setImageLoading}
+                selectedCategory={selectedCategory}
+                buttonText={buttonText}
+              />
             </div>
-            <div  className="-z-[1] absolute left-1 top-[80vh] h-5 w-5">
-                <img src="/images/bean.png" alt=""/>
-            </div>
-            <div  className="-z-[1] absolute right-1 top-[70vh] h-5 w-5">
-                <img src="/images/beans.png" alt=""/>
-            </div>
-            <div  className="-z-[1] absolute right-1 top-[90vh] h-5 w-5">
-                <img src="/images/windmill.png" alt=""/>
-            </div>
+
+            {cards.map((card, index) => {
+              return (
+                <CategoryCard
+                  key={index}
+                  index={index}
+                  category={card.category}
+                  description={card.description}
+                  image={card.image}
+                  setSelectedCategory={setSelectedCategory}
+                  selectedCategory={selectedCategory}
+                />
+              )
+            })}
+          </div>
         </div>
+
+        {/* BG Icons */}
+        <div className="absolute left-1 top-[60vh] -z-[1] h-5 w-5">
+          <img src="/images/windmill.png" alt="" />
+        </div>
+        <div className="absolute left-1 top-[80vh] -z-[1] h-5 w-5">
+          <img src="/images/bean.png" alt="" />
+        </div>
+        <div className="absolute right-1 top-[70vh] -z-[1] h-5 w-5">
+          <img src="/images/beans.png" alt="" />
+        </div>
+        <div className="absolute right-1 top-[90vh] -z-[1] h-5 w-5">
+          <img src="/images/windmill.png" alt="" />
+        </div>
+      </div>
     </>
   )
 }
 
 export default Home
 
-// Drink object 
+// Drink object
 // {
 //   "name": "9-1-1",
 //   "slug": "911",
